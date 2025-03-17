6ed14fdea5dd57c8355e431a577f4ac4f89f99df
Adrielanders <Adrielanderson.s@gmail.com>
2025-03-17 13:59:32 +0700

    baru lagi

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,3 +12,24 @@
     <link href="{{ asset('css\swiper11.css') }}" rel="stylesheet">
     <link href="{{ asset('css\googleapis.css') }}" rel="stylesheet">
 </head>
+
+<body>
+    <div class="navbar">
+        <div class="logo">HR System</div>
+        <ul class="nav-links">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Karyawan</a></li>
+            <li><a href="#">Departemen</a></li>
+            <li><a href="#">Kehadiran</a></li>
+            <li><a href="#">Penggajian</a></li>
+            <li><a href="#">Pengaturan</a></li>
+        </ul>
+        <button class="logout-btn">Logout</button>
+    </div>
+    <div class="content">
+        @yield('body')
+    </div>
+    <footer>
+        <p>&copy; 2025 Kotajati Furindo. All rights reserved.</p>
+    </footer>
+</body>
